707e74f0a9bb35efd8a149f0b5f619af089130d0
Catalyst023 <mzolisi2304@gmail.com>
2021-09-29 20:40:39 +0200

    Added Assets, Columns & Rows

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # example_repo
 Just an example for android
+
+This is the practice for the flutter application.
+
+#1 - Understand the Widget Tree and Element Tree
+#2 - Try to create a AppBar, FloatingActionButton actions and changing activities.
+#3 - Then we'll continue to the next stage
--- sampleproject/lib/main.dart
+++ sampleproject/lib/main.dart
@@ -1,17 +1,61 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-    home: Scaffold(
-        appBar: AppBar(
-          title: Text("My First Flutter App"),
-        ),
-        body: Center(
-          child: Text("Hello Ninjas"),
-        ), 
-        floatingActionButton: FloatingActionButton(
-          child: Text("Click"), 
-          onPressed: null,
-        ),
-    ),
-  ),
-);
+void main() => runApp(MaterialApp(home: Home()));
+
+// ignore: use_key_in_widget_constructors
+class Home extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My First Flutter App"),
+        backgroundColor: Colors.red[600],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+        children: [       
+        Expanded(flex: 1, child: Image.asset("assets/lee.jpg")),
+        
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          Text("Row 1"),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.grey,
+              padding: EdgeInsets.symmetric(),
+              child: Text("Container 1"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.amber,
+              padding: EdgeInsets.symmetric(),
+              child: Text("Container 2"),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.blue,
+              padding: EdgeInsets.symmetric(),
+              child: Text("Container 3"),
+            ),
+          ),
+          
+        ],),
+        
+      ],),
+      floatingActionButton: FloatingActionButton(
+        child: const Text("Click"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
